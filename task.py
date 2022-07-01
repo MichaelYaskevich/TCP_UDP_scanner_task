468126df036b485a71b700ac3fd3ce4002a87bd3
MichaelYaskevich <The_person38@mail.ru>
2022-07-01 21:48:57 +0500

    Сделал таск

---
+++ task.py
@@ -0,0 +1,71 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import socket
+import argparse
+
+
+modes_to_names = {socket.SOCK_STREAM: 'TCP', socket.SOCK_DGRAM: 'UDP'}
+
+
+def is_port_free(mode, address, sock):
+    try:
+        if mode == socket.SOCK_STREAM:
+            sock.connect(address)
+            sock.close()
+            return True
+        elif mode == socket.SOCK_DGRAM:
+            sock.sendto(b'query', address)
+            sock.recvfrom(512)
+            sock.close()
+            return True
+    except OSError:
+        return False
+
+
+def run(host, start_port, end_port):
+    result = []
+    for mode in modes_to_names.keys():
+        result.append(run_with_mode(mode, host, start_port, end_port))
+    return result
+
+
+def run_with_mode(mode, host, start_port, end_port):
+    result = []
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_port = {}
+        for port in range(start_port, end_port):
+            new_socket = socket.socket(socket.AF_INET, mode)
+            future = executor.submit(is_port_free, mode, (host, port), new_socket)
+            future_to_port[future] = port
+
+        for future in as_completed(future_to_port):
+            port = future_to_port[future]
+            try:
+                port_is_free = future.result()
+            except Exception as exc:
+                print('%r generated an exception: %s' % (port, exc))
+            else:
+                result.append((modes_to_names[mode], port, port_is_free))
+    return result
+
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("host", type=str)
+    parser.add_argument("start", type=int)
+    parser.add_argument("end", type=int)
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = make_parser()
+    args = parser.parse_args()
+
+    result = run(args.host, args.start, args.end)
+
+    for ports in result:
+        for port_info in sorted(ports, key=lambda x: x[1]):
+            protocol, port, is_open = port_info
+            print(protocol, port, is_open)
+        print('---------------------------')
